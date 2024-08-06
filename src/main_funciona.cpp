e7a6ac772e442b04c21b38b9fada7fd314fed9c6
Filipe Mendes <107582505+Filipe794@users.noreply.github.com>
2024-08-06 03:31:55 -0300

    ESSE AQ FUNCIONA!

--- src/main_funciona.cpp
+++ src/main_funciona.cpp
@@ -3,27 +3,27 @@
 #include <SoftwareSerial.h>
 #include <Ultrasonic.h>
 
-#define TRIGD 9   // Pino Trig Sensor Direita
-#define ECHOD A4  // Pino Echo Sensor Direita
+#define TRIGD 9  // Pino Trig Sensor Direita
+#define ECHOD A4 // Pino Echo Sensor Direita
 
-#define TRIGC A0  // Pino Trig Sensor Centro
-#define ECHOC A1  // Pino Echo Sensor Centro
+#define TRIGC A0 // Pino Trig Sensor Centro
+#define ECHOC A1 // Pino Echo Sensor Centro
 
-#define TRIGE A2  // Pino Trig Sensor Esquerda
-#define ECHOE A3  // Pino Echo Sensor Esquerda
+#define TRIGE A2 // Pino Trig Sensor Esquerda
+#define ECHOE A3 // Pino Echo Sensor Esquerda
 
-#define ENA 5  // ENA PWM Motor Esquerdo
-#define ENB 6  // ENB PWM Motor Direito
+#define ENA 5 // ENA PWM Motor Esquerdo
+#define ENB 6 // ENB PWM Motor Direito
 
-#define IN1 2  // DIR Motor Esquerdo
-#define IN2 4  // DIR Motor Esquerdo
+#define IN1 2 // DIR Motor Esquerdo
+#define IN2 4 // DIR Motor Esquerdo
 
-#define IN3 7  // DIR Motor Direito
-#define IN4 8  // DIR Motor Direito
+#define IN3 7 // DIR Motor Direito
+#define IN4 8 // DIR Motor Direito
 
-#define POTK A5  // Pino potenciômetro
+#define POTK A5 // Pino potenciômetro
 
-SoftwareSerial bluetooth(8, 9);  // rx, tx amarelo e verde respectivamente
+SoftwareSerial bluetooth(8, 9); // rx, tx amarelo e verde respectivamente
 
 Ultrasonic sensorD(TRIGD, ECHOD);
 Ultrasonic sensorE(TRIGE, ECHOE);
@@ -34,7 +34,7 @@ float distanciaE;
 double distanciaC;
 float distanciaD;
 
-float speed = 1.0;  // throttle in % percent
+float speed = 1.0; // throttle in % percent
 unsigned long time;
 /*Parâmetros para ajustar*/
 
@@ -45,8 +45,9 @@ unsigned long time;
 
 float MAX_DELTA = 40;
 
-float MAX_VOLTAGE = 50;
-float MIN_VOLTAGE = 45;
+float MAX_VOLTAGE = 87;
+float MIN_VOLTAGE_ESQ = 50;
+float MIN_VOLTAGE_DIR = 30;
 
 double DIS_MAX = 7.1;
 double DIS_MIN = 0.1;
@@ -58,32 +59,34 @@ double kpCentral = 5.0, kiCentral = 3.5, kdCentral = 2.0;
 PID PIDc(&distanciaC, &OutputC, &SetpointCentral, kpCentral, kiCentral,
          kdCentral, REVERSE);
 
-void ler_sensores() {
+void ler_sensores()
+{
   long microsec = sensorE.timing();
   distanciaE =
       min(MAX_DELTA + 1.6,
           sensorE.convert(
-              microsec, Ultrasonic::CM));  // filtro(sensorE.convert(microsec,
-                                           // Ultrasonic::CM), distanciaE, 1.0);
+              microsec, Ultrasonic::CM)); // filtro(sensorE.convert(microsec,
+                                          // Ultrasonic::CM), distanciaE, 1.0);
   distanciaE = max(0, distanciaE - 1.6);
 
   microsec = sensorD.timing();
   distanciaD = min(
       MAX_DELTA + 1.5,
       sensorD.convert(microsec,
-                      Ultrasonic::CM));  // filtro(sensorD.convert(microsec,
-                                         // Ultrasonic::CM), distanciaD, 1.0);
+                      Ultrasonic::CM)); // filtro(sensorD.convert(microsec,
+                                        // Ultrasonic::CM), distanciaD, 1.0);
   distanciaD = max(0, distanciaD - 1.5);
 
   microsec = sensorC.timing();
   distanciaC =
       min(MAX_DELTA,
           sensorC.convert(
-              microsec, Ultrasonic::CM));  // filtro(sensorC.convert(microsec,
-                                           // Ultrasonic::CM), distanciaC, 1.0);
+              microsec, Ultrasonic::CM)); // filtro(sensorC.convert(microsec,
+                                          // Ultrasonic::CM), distanciaC, 1.0);
 }
 
-void imprimeDistancias() {
+void imprimeDistancias()
+{
   Serial.print("Dis Esq: ");
   Serial.print(distanciaE);
   Serial.print(" cm  /  ");
@@ -95,20 +98,24 @@ void imprimeDistancias() {
   Serial.println(" cm");
 }
 
-float tratamento(float vel) {
-  if (vel > MAX_VOLTAGE) {
+float tratamento(float vel, float MIN_VOLTAGE)
+{
+  if (vel > MAX_VOLTAGE)
+  {
     vel = MAX_VOLTAGE;
   }
-  if (vel < MIN_VOLTAGE) {
+  if (vel < MIN_VOLTAGE)
+  {
     vel = MIN_VOLTAGE;
   }
   vel = (vel)*MAX_VOLTAGE / 100;
   return vel;
 }
 
-void acelera(float vel_esquerda, float vel_direita) {
-  int vel_direita_int = ceil(tratamento(vel_direita));
-  int vel_esquerda_int = ceil(tratamento(vel_esquerda));
+void acelera(float vel_esquerda, float vel_direita)
+{
+  int vel_direita_int = ceil(tratamento(vel_direita, MIN_VOLTAGE_DIR));
+  int vel_esquerda_int = ceil(tratamento(vel_esquerda, MIN_VOLTAGE_ESQ));
 
   digitalWrite(IN3, HIGH);
   digitalWrite(IN4, LOW);
@@ -119,14 +126,15 @@ void acelera(float vel_esquerda, float vel_direita) {
   analogWrite(ENA, vel_direita_int);
 }
 
-void setup() {
-  Serial.begin(9600);  // Comunicação Serial com o Computador
+void setup()
+{
+  Serial.begin(9600); // Comunicação Serial com o Computador
   pinMode(ENA, OUTPUT);
   pinMode(ENB, OUTPUT);
-  pinMode(IN1, OUTPUT);  // definição dos pinos entradas e saidas
+  pinMode(IN1, OUTPUT); // definição dos pinos entradas e saidas
   pinMode(IN2, OUTPUT);
-  pinMode(IN3, OUTPUT);  // OUTPUT = Saída
-  pinMode(IN4, OUTPUT);  // INPUT = Entrada
+  pinMode(IN3, OUTPUT); // OUTPUT = Saída
+  pinMode(IN4, OUTPUT); // INPUT = Entrada
   pinMode(TRIGD, OUTPUT);
   pinMode(ECHOD, INPUT);
   pinMode(TRIGE, OUTPUT);
@@ -135,9 +143,9 @@ void setup() {
   pinMode(ECHOC, INPUT);
   pinMode(POTK, INPUT);
 
-  PIDc.SetMode(AUTOMATIC);
-  PIDc.SetTunings(kpCentral, kiCentral, kdCentral);
-  PIDc.SetOutputLimits(MIN_VOLTAGE, MAX_VOLTAGE);
+  // PIDc.SetMode(AUTOMATIC);
+  // PIDc.SetTunings(kpCentral, kiCentral, kdCentral);
+  // PIDc.SetOutputLimits(MIN_VOLTAGE, MAX_VOLTAGE);
 
   ler_sensores();
   delay(2000);
@@ -149,56 +157,66 @@ void setup() {
 // unsigned long time_here_right = 0;
 // unsigned long last_time = 0;
 
-double output_func_math(int choice, double referencia,
-                        double referencia_2 = 0) {
+double output_func_math(int choice, float MIN_VOLTAGE, double referencia,
+                        double referencia_2 = 0)
+{
   double c;
   double a;
-  switch (choice) {
-    // cosseno
-    case 0:
-      return cos(referencia * acos(MIN_VOLTAGE / MAX_VOLTAGE) / DIS_MAX) *
-             MAX_VOLTAGE;
+  switch (choice)
+  {
+  // cosseno
+  case 0:
+    return cos(referencia * acos(MIN_VOLTAGE / MAX_VOLTAGE) / DIS_MAX) *
+           MAX_VOLTAGE;
 
-    // quadratica
-    case 1:
-      c = MAX_VOLTAGE;
-      a = (MIN_VOLTAGE - MAX_VOLTAGE) / DIS_MAX * DIS_MAX;
-      return referencia * referencia * a + c;
+  // quadratica
+  case 1:
+    c = MAX_VOLTAGE;
+    a = (MIN_VOLTAGE - MAX_VOLTAGE) / DIS_MAX * DIS_MAX;
+    return referencia * referencia * a + c;
 
-    // linear
-    case 2:
-      return ceil((-50.0 / 15.5) * referencia + 100.0);
+  // linear
+  case 2:
+    // valores iniciais a = -50.0 e c = 15.5
+    a = -70;
+    c = 16;
+    return ceil((a / c) * referencia + 100.0);
 
-    // 3 variaveis, varia em relacao ao tempo
-    case 3:
-      c = MAX_VOLTAGE;
-      a = (MIN_VOLTAGE - MAX_VOLTAGE) / DIS_MAX * DIS_MAX;
-      if (a < 1) {
-        return referencia * referencia * a * referencia_2 + c;
-      } else {
-        return referencia * referencia * a / referencia_2 + c;
-      }
+  // 3 variaveis, varia em relacao ao tempo
+  case 3:
+    c = MAX_VOLTAGE;
+    a = (MIN_VOLTAGE - MAX_VOLTAGE) / DIS_MAX * DIS_MAX;
+    if (a < 1)
+    {
+      return referencia * referencia * a * referencia_2 + c;
+    }
+    else
+    {
+      return referencia * referencia * a / referencia_2 + c;
+    }
 
-    // erro
-    default:
-      return -1;
+  // erro
+  default:
+    return -1;
   }
 }
 
-void ajuste(float delta) {
+void ajuste(float delta)
+{
   // variável que calcula o quanto uma roda deverá diminuir para ajustar o
   // carrinho
-
-  double valor_acelera = output_func_math(1, abs(delta));
+  double valor_acelera;
 
   // CONTROLE PID
   // valor_acelera *= OutputC / MAX_VOLTAGE;
 
-  valor_acelera = min(valor_acelera, 100);
-  valor_acelera = max(valor_acelera, 0);
   // float valor_acelera =
   //  mais a direita
-  if (delta > 0) {
+  if (delta > 0)
+  {
+    valor_acelera = output_func_math(2, MIN_VOLTAGE_ESQ, abs(delta));
+    valor_acelera = min(valor_acelera, 100);
+    valor_acelera = max(valor_acelera, 0);
     // time_here_right += (millis() - last_time);
     // time_here_left = 0;
     //  digitalWrite(led_amarelo_esquerda, HIGH);
@@ -207,13 +225,19 @@ void ajuste(float delta) {
   }
 
   // mais a esquerda
-  else if (delta < 0) {
+  else if (delta < 0)
+  {
+    valor_acelera = output_func_math(2, MIN_VOLTAGE_DIR, (delta));
+    valor_acelera = min(valor_acelera, 100);
+    valor_acelera = max(valor_acelera, 0);
     // time_here_left += (millis() - last_time);
     // time_here_right = 0;
     //  digitalWrite(led_amarelo_esquerda, LOW);
     //  digitalWrite(led_azul_direita, HIGH);
-    acelera(100, valor_acelera);
-  } else {
+    acelera(100, valor_acelera*0.20);
+  }
+  else
+  {
     // time_here_left = 0;
     // time_here_right = 0;
     //  digitalWrite(led_amarelo_esquerda, LOW);
@@ -224,7 +248,8 @@ void ajuste(float delta) {
 }
 
 // THROTTLE COM FUNÇÃO QUADRATICA QUE NAO SABEMOS COMO, MAS FUNCIONA
-void throttle() {
+void throttle()
+{
   float a = 2.0 / 18.0;
   // float b = 2.0/3.0;
   // float valorC = ((a * distanciaC * distanciaC) - (b * distanciaC))/100;
@@ -233,7 +258,8 @@ void throttle() {
   // int delta = distanciaE - distanciaD;
 
   float step = 0.0;
-  if (delta_time > 50) {
+  if (delta_time > 50)
+  {
     time = millis();
     step = sqrt(speed / 100.0);
     step += 0.002;
@@ -262,11 +288,13 @@ void throttle() {
   }
 }
 
-void loop() {
-  while (true) {
+void loop()
+{
+  while (true)
+  {
     ler_sensores();
     PIDc.Compute();
-    throttle();
+    // throttle();
     float delta = distanciaE - distanciaD;
     ajuste(delta);
   }
